1da71b436fce233c079569a6ee23bfcc8825b9b2
A707\dam1 <cristiansanchezmad@gmail.com>
2025-10-17 19:20:13 +0200

    no message

--- src/main/java/org/example/Actividadgeneraciones.java
+++ src/main/java/org/example/Actividadgeneraciones.java
@@ -1,4 +1,78 @@
 package org.example;
 
+import java.time.LocalDateTime;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Actividadgeneraciones {
+    static void main() {
+        Scanner telcado = new Scanner(System.in);
+        boolean seguir = true;
+        final int ANYO_MINIMO = 1900;
+        int modo = 0;
+        int anyo_actual = 0;
+        int anyo_nacimiento_int = 0;
+        System.out.println("Elige un modo...");
+        System.out.println("[1] - Año de nacimiento");
+        System.out.println("[2] - Edad");
+        System.out.println("----------------------------");
+        if (telcado.hasNext()) {
+            modo = telcado.nextInt();
+        } else {
+            System.out.println("El valor del modo incorrecto");
+            seguir = false;
+        }
+        if (seguir) {
+            if (modo == 1) {
+                System.out.println("Introduce tu año de nacimiento:");
+                String anyo_nacimiento = telcado.next();
+
+                try {
+                    anyo_nacimiento_int = Integer.parseInt((anyo_nacimiento));
+                } catch (NumberFormatException e) {
+                    System.out.println("El formato no es numérico.");
+                    return;
+                }
+                LocalDateTime fecha = LocalDateTime.now();
+                anyo_actual = fecha.getYear();
+
+            } else if (modo == 2) {
+                int edad = 0;
+
+                if (telcado.hasNextInt()) {
+                    edad = telcado.nextInt();
+                } else {
+                    System.out.println("La edad no tiene formato correcto (numérico)");
+                    return;
+                }
+                if (edad >= 0) {
+                    anyo_nacimiento_int = (anyo_actual - edad);
+                } else {
+                    System.out.println("La edad no es corrrecta.");
+                }
+            } else {
+                System.out.println("El modo introducidp no es correcto");
+                return;
+            }
+            if (anyo_nacimiento_int >= ANYO_MINIMO && anyo_nacimiento_int <= anyo_actual) {
+
+                if (anyo_nacimiento_int <= 1927) {
+                    System.out.println("Generación sin bautizar");
+                } else if (anyo_nacimiento_int >= 1928 && anyo_nacimiento_int <= 1944) {
+                    System.out.println("Generación silent");
+                } else if (anyo_nacimiento_int >= 1945 && anyo_nacimiento_int <= 1964) {
+                    System.out.println("Baby boomer");
+                } else if (anyo_nacimiento_int >= 1965 && anyo_nacimiento_int <= 1982) {
+                    System.out.println("Generacion X");
+                } else if (anyo_nacimiento_int >= 1982 && anyo_nacimiento_int <= 1994) {
+                    System.out.println("Millenials");
+                } else {
+                    System.out.println("Generación Z");
+                }
+            }
+        }
+    }
 }
+
+
+
--- src/main/java/org/example/CosasHasNexIn.java
+++ src/main/java/org/example/CosasHasNexIn.java
@@ -1,4 +1,16 @@
 package org.example;
 
-public class ActividadHasNexIn {
-}
+import java.util.Scanner;
+
+public class CosasHasNexIn {
+    static void main() {
+        Scanner teclado = new Scanner(System.in);
+        if (teclado.hasNextInt()) {
+            int numero = teclado.nextInt();
+            System.out.println(numero);
+        } else {
+            System.out.println("Formato incorrectp. No es un número");
+        }
+
+    }
+    }
